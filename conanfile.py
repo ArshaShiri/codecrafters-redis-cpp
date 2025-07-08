85e865ff7aa89f332e1100b8269f526542d5d036
Arsha <arsha.shiri@gmail.com>
2025-07-08 20:28:19 +0200

    change the layout and use different build system and directory structure

---
+++ conanfile.py
@@ -0,0 +1,14 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+class Starter(ConanFile):
+    name = "redis"
+    version = "0.1"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires("gtest/1.14.0")
